abb943fdeaf9442be3fe945609bd81552bf2b5e3
neaorin <neaorin@gmail.com>
2016-04-20 15:05:35 +0300

    push no2

--- tables/TodoItem.js
+++ tables/TodoItem.js
@@ -6,32 +6,32 @@ var table = azureMobileApps.table();
 
 table.insert(function (context) {
 
-logger.info('Running TodoItem.insert');
+    logger.info('Running TodoItem.insert');
 
-// Define the template payload.
-var payload = '{"messageParam": context.item.text}'; 
+    // Define the template payload.
+    var payload = '{"messageParam": context.item.text}'; 
 
-// Execute the insert.  The insert returns the results as a Promise,
-// Do the push as a post-execute action within the promise flow.
-return context.execute()
-    .then(function (results) {
-        // Only do the push if configured
-        if (context.push) {
-            // Send a template notification.
-            context.push.send(null, payload, function (error) {
-                if (error) {
-                    logger.error('Error while sending push notification: ', error);
-                } else {
-                    logger.info('Push notification sent successfully!');
-                }
-            });
-        }
-        // Don't forget to return the results from the context.execute()
-        return results;
-    })
-    .catch(function (error) {
-        logger.error('Error while running context.execute: ', error);
-    });
+    // Execute the insert.  The insert returns the results as a Promise,
+    // Do the push as a post-execute action within the promise flow.
+    return context.execute()
+        .then(function (results) {
+            // Only do the push if configured
+            if (context.push) {
+                // Send a template notification.
+                context.push.send(null, payload, function (error) {
+                    if (error) {
+                        logger.error('Error while sending push notification: ', error);
+                    } else {
+                        logger.info('Push notification sent successfully!');
+                    }
+                });
+            }
+            // Don't forget to return the results from the context.execute()
+            return results;
+        })
+        .catch(function (error) {
+            logger.error('Error while running context.execute: ', error);
+        });
 });
 
 module.exports = table;  
