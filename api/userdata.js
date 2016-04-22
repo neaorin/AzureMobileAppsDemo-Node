6dca725e2a5be0e3cce85c7997c8db396bbfa189
neaorin <neaorin@gmail.com>
2016-04-22 23:52:58 +0300

    userdata api

--- api/userdata.js
+++ api/userdata.js
@@ -1,33 +1,10 @@
 var api = {
-    get: function (request, response, next) {
-        var user = request.azureMobile.user;
-        user.getIdentities({
-        success: function(identities) {
-            var accessToken = identities.microsoft.accessToken;
-            var url = 'https://apis.live.net/v5.0/me/?method=GET&access_token=' + accessToken;
-            var requestCallback = function (err, resp, body) {
-                if (err || resp.statusCode !== 200) {
-                    console.error('Error sending data to the provider: ', err);
-                    response.send(statusCodes.INTERNAL_SERVER_ERROR, body);
-                } else {
-                    try {
-                        var userData = JSON.parse(body);
-                        response.send(200, userData);
-                    } catch (ex) {
-                        console.error('Error parsing response from the provider API: ', ex);
-                        response.send(statusCodes.INTERNAL_SERVER_ERROR, ex);
-                    }
-                }
-            }
-            var req = require('request');
-            var reqOptions = {
-                uri: url,
-                headers: { Accept: "application/json" }
-            };
-            req(reqOptions, requestCallback);
-        }
-    });
-
+    "get": function (req, res, next) {
+        req.azureMobile.user.getIdentity("microsoft").then((data) => {
+            res.status(200).type('application/json').json(data);
+        }).catch((error) => {
+            res.status(500).send(JSON.stringify(error));
+        });
     }
 };
 
