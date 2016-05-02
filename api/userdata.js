72fd4264c9ff401e760f7864fd27cf805c37e344
neaorin <neaorin@gmail.com>
2016-05-02 23:23:11 +0300

    userdata cleanup

--- api/userdata.js
+++ api/userdata.js
@@ -1,7 +1,6 @@
 var api = {
     "get": function (req, res, next) {
         req.azureMobile.user.getIdentity().then((data) => {
-            logger.info(JSON.stringify(data));
             res.status(200).type('application/json').json(data);
         }).catch((error) => {
             res.status(500).send(JSON.stringify(error));
